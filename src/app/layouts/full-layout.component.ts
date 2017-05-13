75d85c1227788bc3c81473a79a9adfb411fd16f8
ijaewung <ijaewung@outlook.com>
2017-05-13 19:59:23 +0900

    new navigator

--- src/app/layouts/full-layout.component.ts
+++ src/app/layouts/full-layout.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FullLayoutComponent implements OnInit {
 
+  public menus: MenuModel[];
+  public navis: NaviModel[];
+  public title = "DOTVEL";
   public disabled = false;
   public status: {isopen: boolean} = {isopen: false};
 
@@ -20,4 +23,32 @@ export class FullLayoutComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  public constructor() {
+    this.title = ".net Avengers";
+
+    var dashboard: MenuModel = { header: "Dashboard", url: "", tooltip: "", number: 0, isenabled: true};
+    var users: MenuModel = { header: "Users", url: "", tooltip: "", number: 0, isenabled: true};
+    var settings: MenuModel = { header: "Settings", url: "", tooltip: "", number: 0, isenabled: true};
+    this.menus = [dashboard, users, settings];
+    var navi_1: NaviModel = { header: "닷넷프레임워크", items: [ { name: "Tutorial" }, { name: "Article"}] };
+    var navi_2: NaviModel = { header: "넷플릭스", items: [ { name: "House of Cards" }, { name: "Defenders"}] };
+    this.navis = [navi_1, navi_2];
+  }
+}
+
+class MenuModel {
+  header: string;
+  url: string;
+  tooltip: string;
+  number: number;
+  isenabled: boolean;
+}
+
+class NaviModel {
+  header: string;
+  items: NaviItemModel[];
+}
+class NaviItemModel {
+  name: string;
 }
